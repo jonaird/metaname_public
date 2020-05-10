d12dc872e7904d4f84c85f04d27ae51e409aacc9
Jonathan Aird <jonathan.aird@gmail.com>
2020-05-10 16:10:09 +0300

    Initial commit

---
+++ flutter_service_worker.js
@@ -0,0 +1,44 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "index.html": "8320ef698bfb2183967071d319c7077e",
+"/": "8320ef698bfb2183967071d319c7077e",
+"main.dart.js": "68c58b9527b8600be59285b16c2ad1a0",
+"favicon.png": "62a5cbc7483ac6dd53fb5e3f428fc7db",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"manifest.json": "27bdfd058cfa55a8a726b364029389db",
+"assets/LICENSE": "71ddbcb846bd75b203d5e4b05a17875a",
+"assets/AssetManifest.json": "9a7e35508bd4bfbed7b9d56936f4c715",
+"assets/FontManifest.json": "48092a100b610a70b734a6568c2ceee8",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "2aa350bd2aeab88b601a593f793734c0",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "2bca5ec802e40d3f4b60343e346cedde",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "5a37ae808cf9f652198acde612b5328d",
+"assets/packages/flutter_markdown/assets/logo.png": "67642a0b80f3d50277c44cde8f450e50",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
